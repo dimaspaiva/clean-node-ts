b562bd7b47c37644564882ca3b403ca357fc47c5
dimas <dimasalpaiva@gmail.com>
2022-02-28 19:17:03 -0300

    feat: ensure sign up controller when invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/emailValidator";
 import { HTTPRequest, HTTPResponse } from "../protocols/http";
 
 export class SignupController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HTTPRequest): HTTPResponse {
     const requiredFields = [
       "name",
@@ -16,6 +24,14 @@ export class SignupController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isEmailValid = this.emailValidator.isValid(
+      httpRequest.body.email
+    );
+
+    if (!isEmailValid) {
+      return badRequest(new InvalidParamError("email"));
     }
   }
 }
