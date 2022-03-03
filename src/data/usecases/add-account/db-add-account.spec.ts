1f7f4324f7ada33496deec45fcf688545ac6dce6
dimas <dimasalpaiva@gmail.com>
2022-03-03 02:09:25 -0300

    refactor: remove duplicated variable

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -47,15 +47,16 @@ const makeSut = (): SutTypes => {
   };
 };
 
+const accountData = {
+  name: "valid name",
+  email: "valid_email",
+  password: "valid_password",
+};
+
 describe("DbAccount Usecase", () => {
   it("should call encrypter with correct password", async () => {
     const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
-    const accountData = {
-      name: "valid name",
-      email: "valid_email",
-      password: "valid_password",
-    };
 
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
@@ -67,12 +68,6 @@ describe("DbAccount Usecase", () => {
       .spyOn(encrypterStub, "encrypt")
       .mockRejectedValueOnce(new Error());
 
-    const accountData = {
-      name: "valid name",
-      email: "valid_email",
-      password: "valid_password",
-    };
-
     const promise = sut.add(accountData);
     await expect(promise).rejects.toThrow();
   });
@@ -80,11 +75,6 @@ describe("DbAccount Usecase", () => {
   it("should call AddAccountRepository with correct values", async () => {
     const { sut, addAccountRepositoryStub } = makeSut();
     const addSpy = jest.spyOn(addAccountRepositoryStub, "add");
-    const accountData = {
-      name: "valid name",
-      email: "valid_email",
-      password: "valid_password",
-    };
 
     await sut.add(accountData);
     expect(addSpy).toHaveBeenCalledWith({
@@ -99,23 +89,12 @@ describe("DbAccount Usecase", () => {
       .spyOn(addAccountRepositoryStub, "add")
       .mockRejectedValueOnce(new Error());
 
-    const accountData = {
-      name: "valid name",
-      email: "valid_email",
-      password: "valid_password",
-    };
-
     const promise = sut.add(accountData);
     await expect(promise).rejects.toThrow();
   });
 
   it("should return an account on success", async () => {
     const { sut } = makeSut();
-    const accountData = {
-      name: "valid name",
-      email: "valid_email",
-      password: "valid_password",
-    };
 
     const account = await sut.add(accountData);
     expect(account).toEqual({
