e9ada948ad6f52a6cfc6736b10e638139a10c7dd
Dimas Paiva <dimas.paiva@luizalabs.com>
2022-03-04 18:36:14 -0300

    feat: ensure mongo db insert an documento in to db

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/useCases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository{
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getInstance().getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    return await accountCollection.findOne<AccountModel>({ _id: result.insertedId });
+  }
+}
