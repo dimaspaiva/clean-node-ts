4aedbe78501693d190cf1b76019a975817005200
Dimas Paiva <dimas.paiva@luizalabs.com>
2022-03-04 18:33:37 -0300

    refactor: define mongo helper as singleton

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,27 @@
-import { MongoClient } from "mongodb"
+import { Collection, MongoClient } from "mongodb"
 
-export const MongoHelper = {
-  client: null as MongoClient,
+export class MongoHelper {
+  private static instance: MongoHelper;
+  private client: MongoClient;
+
+  private constructor() {}
+
+  public static getInstance() {
+    if(!this.instance) {
+      this.instance = new MongoHelper(); 
+    }
+    return this.instance;
+  }
+
   async connect (url: string) {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-  },
-  async disconnect() {
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
+  }
+
+  async disconnect () {
     await this.client.close();
+  }
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
