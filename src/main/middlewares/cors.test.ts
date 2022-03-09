c91b92651dab6406c64089343727294787c71559
dimas <dimasalpaiva@gmail.com>
2022-03-09 04:57:10 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+
+import app from "../config/app";
+
+describe("Cors", () => {
+  it("should ensure cors is working", async () => {
+    app.get("/test_cors", (req, res) => {
+      res.send();
+    });
+
+    const response = await request(app).get("/test_cors");
+
+    expect(response.headers).toHaveProperty("access-control-allow-origin");
+    expect(response.headers["access-control-allow-origin"]).toEqual("*");
+
+    expect(response.headers).toHaveProperty(
+      "access-control-allow-methods"
+    );
+    expect(response.headers["access-control-allow-methods"]).toEqual("*");
+
+    expect(response.headers).toHaveProperty(
+      "access-control-allow-headers"
+    );
+    expect(response.headers["access-control-allow-headers"]).toEqual("*");
+  });
+});
