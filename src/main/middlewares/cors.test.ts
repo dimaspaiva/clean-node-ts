79e739f1662e0cb8c3d2061e7a76d93c2f238a84
dimas <dimasalpaiva@gmail.com>
2022-03-09 04:59:56 -0300

    chore: rename describe cors test

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 
 import app from "../config/app";
 
-describe("Cors", () => {
+describe("Cors middleware", () => {
   it("should ensure cors is working", async () => {
     app.get("/test_cors", (req, res) => {
       res.send();
