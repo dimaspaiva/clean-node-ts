0c2c84901a5532437875cbe5fb966c0cfe266d85
dimas <dimasalpaiva@gmail.com>
2022-03-09 22:53:55 -0300

    refactor: connect to mongo db before integration tests

--- src/main/routes/sigunp.test.ts
+++ src/main/routes/sigunp.test.ts
@@ -1,8 +1,24 @@
 import request from "supertest";
 
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
 
+const mongoHelper = MongoHelper.getInstance();
+
 describe("Signup routes", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
   it("Should ensure signup route is working", async () => {
     const response = await request(app).post("/api/signup").send({
       name: "Dimas",
